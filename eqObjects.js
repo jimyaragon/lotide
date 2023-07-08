4e8a9f19479e4f6cd9351d258c13f787b113ec35
jimyaragon <jimy.aragon@gmail.com>
2023-07-08 12:02:26 -0600

    Adding AssertObject

---
+++ eqObjects.js
@@ -0,0 +1,33 @@
+const assertEqual = function(actual, expected) {
+    if (actual === expected) {
+        console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+    } else {
+        console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+    }
+}
+
+const eqObjects = function(object1, object2) {
+    const keys1 = Object.keys(object1);
+    const keys2 = Object.keys(object2);
+  
+    if (keys1.length !== keys2.length) {
+      return false;
+    }
+  
+    for (const key of keys1) {
+      if (object1[key] !== object2[key]) {
+        return false;
+      }
+    }
+  
+    return true;
+  };
+  
+
+const shirtObject = { color: "red", size: "medium" };
+const anotherShirtObject= { size: "medium", color: "red" };
+eqObjects(shirtObject , anotherShirtObject); // => true
+
+const longSleeveShirtObject= { size: "medium", color: "red", sleeveLength: "long" };
+eqObjects(shirtObject , longSleeveShirtObject); // => false
+assertEquals(eqObjects(shirtObject , longSleeveShirtObject), false);
